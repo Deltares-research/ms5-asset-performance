Fixed LSF Fragility curve calculation works

src/reliability_models/dsheetpiling/lsf.py
@@ -3,8 +3,6 @@ from numpy.typing import NDArray
 from src.geotechnical_models.dsheetpiling.model import *
 from src.rvs.state import *
 from typing import Optional, Type, Tuple, Dict, Callable
-import itertools
-import inspect
 
 
 LSFInputType = Annotated[Tuple[float,...] | List[float] | NDArray[np.float64], "lsf_input"]
@@ -25,14 +23,21 @@ def lsf({args_str}):
 """
     namespace = {'body_func': body_func}
     exec(func_code, namespace)
-    return namespace['lsf']
+    fn = namespace['lsf']
+    fn._generated_source = func_code
+    return fn
 
 
-def unpack_params(params: Dict[str, float]) -> Dict[str, Dict[str, float]]:
+def unpack_params(params: Dict[str, float], soil_layers: List[str]) -> Dict[str, Dict[str, float]]:
     soil_data = {}
     for (key, val) in params.items():
-        soil_name = key.split("_")[0]
-        param_name = key.split("_")[1]
+        try:
+            soil_name = key.split("_")[0]
+            param_name = key.split("_")[1]
+        except:
+            continue
+        if not soil_name in soil_layers:
+            continue
         param_value = val
         if not soil_name in list(soil_data.keys()):
             soil_data[soil_name] = {param_name: float(param_value)}
@@ -45,7 +50,7 @@ def unpack_params(params: Dict[str, float]) -> Dict[str, Dict[str, float]]:
 def safety_fn(
         params: Dict[str, float],
         geomodel: DSheetPiling,
-        rv: Type[RV],
+        state: Type[StateBase],
         performance_config: Tuple[str, Callable[[float | List[float]], float]],
         standardized_rv: bool = False
 ) -> float:
@@ -55,12 +60,13 @@ def safety_fn(
     TODO: Use something less common that _?
     """
 
+    rvs = {key: param for (key, param) in params.items() if key in state.names}
     if standardized_rv:
-        x_st = np.asarray(list(params.values()))
-        x = rv.transform(x_st)
-        params = {key: val for (key, val) in zip(params.keys(), x)}
+        x_st = np.asarray(list(rvs.values()))
+        x = state.transform(x_st)
+        rvs = {key: val for (key, val) in zip(rvs.keys(), x)}
 
-    soil_data = unpack_params(params)
+    soil_data = unpack_params(rvs, list(geomodel.soils.keys()))
     geomodel.update_soils(soil_data)
     geomodel.execute()
     results = geomodel.results
@@ -74,8 +80,7 @@ def safety_fn(
 
 def package_lsf(
         geomodel: DSheetPiling,
-        soil_layers: Dict[str, Tuple[str, ...]],
-        rv: Type[RV],
+        state: Type[StateBase],
         performance_config: Tuple[str, Callable[[float | List[float]], float]],
         standardized_rv: bool = False
 ) -> LSFType:
@@ -84,21 +89,18 @@ def package_lsf(
     if not model_parsed:
         raise ValueError("Geotechnical model has not yet been parsed.")
 
-    """ Check is soil layers and parameters exist in model """
-    geomodel_soil_names = geomodel.get_soils()
-    for soil_name, soil_params in soil_layers.items():
-        if soil_name not in list(geomodel_soil_names.keys()):
-            raise ValueError(f"Soil name {soil_name} not found in geomodel.")
-        geomodel_soil = geomodel_soil_names[soil_name]
-        geomodel_layer_params = list(vars(geomodel_soil).keys())
-        for soil_param in soil_params:
-            if soil_param not in geomodel_layer_params:
-                raise ValueError(f"Soil parameter {soil_param} of soil {soil_name} not found in geomodel.")
+    # """ Check is soil layers and parameters exist in model """
+    # geomodel_soil_names = geomodel.get_soils()
+    # for soil_name, soil_params in soil_layers.items():
+    #     if soil_name not in list(geomodel_soil_names.keys()):
+    #         raise ValueError(f"Soil name {soil_name} not found in geomodel.")
+    #     geomodel_soil = geomodel_soil_names[soil_name]
+    #     geomodel_layer_params = list(vars(geomodel_soil).keys())
+    #     for soil_param in soil_params:
+    #         if soil_param not in geomodel_layer_params:
+    #             raise ValueError(f"Soil parameter {soil_param} of soil {soil_name} not found in geomodel.")
 
-    rvs = [[name+"_"+param for param in soil_layers[name]] for name in soil_layers.keys()]
-    rvs = list(itertools.chain(*rvs))
-
-    lsf = build_lsf(rvs, lambda x: safety_fn(x, geomodel, rv, performance_config, standardized_rv))
+    lsf = build_lsf(state.names, lambda x: safety_fn(x, geomodel, state, performance_config, standardized_rv))
 
     return lsf
 
@@ -109,14 +111,16 @@ if __name__ == "__main__":
     form_path = os.environ["FORM_PATH"]
     geomodel = DSheetPiling(geomodel_path, form_path)
 
-    soil_layers = {"Klei": ("soilphi", "soilcohesion")}
-    state = GaussianState(rvs=[MvnRV(mus=[30, 10], stds=[3, 1], names=["Klei_soilphi", "Klei_soilcohesion"])])
+    state = GaussianState(rvs=[
+        MvnRV(mus=[30, 10], stds=[3, 1], names=["Klei_soilphi", "Klei_soilcohesion"]),
+        MvnRV(mus=[1], stds=[0.1], names=["water_level"])  # Unused, just for testing
+    ])
     performance_config = ("max_moment", lambda x: 150. / (x[0] + 1e-5))
 
-    """ The first arg is "soilphi" and the second is "soilcohesion" for "Klei", as declared. """
-    lsf = package_lsf(geomodel, soil_layers, state.rvs[0], performance_config, False)
-    limit_state = lsf(30, 10)
+    """ The args are "soilphi", "soilcohesion" and "water_lvl" respectively. """
+    lsf = package_lsf(geomodel, state, performance_config, False)
+    limit_state = lsf(30, 10, 1)
 
-    lsf = package_lsf(geomodel, soil_layers, state.rvs[0], performance_config, True)
-    limit_state_st = lsf(0, 0)
+    lsf_st = package_lsf(geomodel, state, performance_config, True)
+    limit_state_st = lsf_st(0, 0, 0)
 

src/reliability_models/dsheetpiling/reliability.py
@@ -1,99 +1,111 @@
 import numpy as np
+from scipy import stats
 import probabilistic_library as ptk
 from src.reliability_models.base import ReliabilityBase
+from src.reliability_models.dsheetpiling.lsf import package_lsf, LSFType
 from src.geotechnical_models.dsheetpiling.model import *
 from src.rvs.state import *
 from typing import Optional, Type, Tuple, Dict
+from tqdm import tqdm
 
 
-class DsheetReliability:
+def init_project(lsf: LSFType, form_params: Tuple[float, int, float] = (0.15, 5, 0.02)) -> ptk.ReliabilityProject:
+    project = ptk.ReliabilityProject()
+    project.model = lsf
+    project.settings.reliability_method = ptk.ReliabilityMethod.form
+    relaxation_factor, maximum_iterations, variation_coefficient = form_params
+    project.settings.relaxation_factor = relaxation_factor
+    project.settings.maximum_iterations = maximum_iterations
+    project.settings.variation_coefficient = variation_coefficient
+    return project
 
-    def __init__(self, geomodel: DSheetPilingModel) -> None:
-        self.geomodel = geomodel
+
+def set_fragility_rvs(project: ptk.ReliabilityProject, rv_names: List[str]) -> ptk.ReliabilityProject:
+    for rv_name in rv_names:
+        if state.marginal_pdf_type[rv_name] in ["normal", "multivariate_normal"]:
+            project.variables[rv_name].distribution = ptk.DistributionType.normal
+            project.variables[rv_name].mean = 0
+            project.variables[rv_name].deviation = 1
+        else:
+            raise NotImplementedError("Non-normal distributions have not been implemented yet.")
+    return project
 
 
-def hunt(t_p, tan_alpha, h_s, h_crest, h):
-    g = 9.81
-    l_0 = g  * t_p * t_p
-    xi = tan_alpha / np.sqrt(2 * np.pi * h_s / l_0)
-    r_u = xi * h_s
+def fragility_point(
+        project: ptk.ReliabilityProject,
+        point: Annotated[NDArray[float], "integration_dims"],
+        integration_rv_names: List[str]
+) -> Tuple[ptk.FragilityValue, float]:
 
-    return h_crest - (h + r_u)
+    for i, rv_name in enumerate(integration_rv_names):
+        project.variables[rv_name].distribution = ptk.DistributionType.deterministic
+        project.variables[rv_name].mean = float(point[i])
+
+    project.run()
+    dp = project.design_point
+
+    frag_value = ptk.FragilityValue()
+    frag_value.x = point
+    frag_value.reliability_index = dp.reliability_index
+    frag_value.design_point = dp
+    fragility_logpf = np.log(dp.probability_failure).item()
+
+    return frag_value, fragility_logpf
 
 
 def build_fragility(
-        lsf,
+        lsf: LSFType,
         state: Type[StateBase],
         fragility_rv_names: Optional[Annotated[Tuple[str, ...] | List[str], "fragility_dims"]] = None,
         integration_rv_names: Optional[Annotated[Tuple[str, ...] | List[str], "integration_dims"]] = None,
+        form_params: Tuple[float, int, float] = (0.15, 5, 0.02),
         n_integration_grid: int = 10
-) -> None:
+) -> ptk.FragilityCurve:
 
     if (fragility_rv_names is None) == (integration_rv_names is None):
         raise ValueError("You must provide exactly one of 'fragility_rv_names' or 'integration_rv_names'")
 
     if fragility_rv_names is None:
         fragility_rv_names = [rv_name for rv_name in state.names if rv_name not in integration_rv_names]
-        fragility_rv_idx = np.asarray([i for i, name in enumerate(state.names) if name in fragility_rv_names])
 
     if integration_rv_names is None:
         integration_rv_names = [rv_name for rv_name in state.names if rv_name not in fragility_rv_names]
-        integration_rv_idx = np.asarray([i for i, name in enumerate(state.names) if name in integration_rv_names])
 
-    project = ptk.ReliabilityProject()
-    project.model = lsf
-    project.settings.reliability_method = ptk.ReliabilityMethod.form
-    project.settings.relaxation_factor = 0.15
-    project.settings.maximum_iterations = 50
-    project.settings.variation_coefficient = 0.02
+    project = init_project(lsf, form_params)
 
-    for rv_name in fragility_rv_names:
-        if state.marginal_dist_type[rv_name] in ["normal", "multivariate_normal"]:
-            project.variables[rv_name+"_stdandard"].distribution = ptk.DistributionType.normal
-            project.variables[rv_name+"_stdandard"].mean = 0
-            project.variables[rv_name+"_stdandard"].deviation = 1
-        else:
-            raise NotImplementedError("Non-normal distributions have not been implemented yet.")
+    project = set_fragility_rvs(project, fragility_rv_names)
 
     cdf_grid = np.linspace(1e-5, 1-1e-5, n_integration_grid)
-    grids = []
-    for rv_name in integration_rv_names:
-        grids.append(state.marginal_dist[rv_name].ppf(cdf_grid))
-    mesh = np.meshgrid(grids)
+    grid = stats.norm(0, 1).ppf(cdf_grid)
+    mesh = np.meshgrid([grid]*len(integration_rv_names))
     mesh = np.c_[*mesh]
 
     fragility_curve = ptk.FragilityCurve()
     fragility_curve.name = "conditional"
-    fragility_logpf = []
-    for val in mesh:
-        project.variables["h"].distribution = ptk.DistributionType.deterministic
-        project.variables["h"].mean = val
-        project.run()
-        dp = project.design_point
+    fragility_logpfs = []
+    for point in tqdm(mesh, desc="Running FORM for combination of integration variables:"):
+        frag_value, fragility_logpf = fragility_point(project, point, integration_rv_names)
+        fragility_curve.fragility_values.append(frag_value)
+        fragility_logpfs.append(fragility_logpf)
 
-        value = ptk.FragilityValue()
-        value.x = val
-        value.reliability_index = dp.reliability_index
-        value.design_point = dp
-
-        fragility_curve.fragility_values.append(value)
-        fragility_logpf.append(np.log(dp.probability_failure).item())
-
-
-    pass
-
+    return fragility_curve
 
 
 if __name__ == "__main__":
 
-    pass
+    geomodel_path = os.environ["MODEL_PATH"]
+    form_path = os.environ["FORM_PATH"]
+    geomodel = DSheetPiling(geomodel_path, form_path)
 
-    model_path = os.environ["MODEL_PATH"]
-    model = DSheetPiling(model_path)
+    soil_layers = {"Klei": ("soilphi", "soilcohesion")}
 
-    rv_strength = MvnRV(mus=np.array([30, 10]), stds=np.array([3, 1]), names=["phi", "c"])
-    rv_water = MvnRV(mus=np.array([-0.8]), stds=np.array([0.08]), names=["water"])
-    state = GaussianState([rv_strength, rv_water])
+    rv_strength = MvnRV(mus=np.array([30, 10]), stds=np.array([3, 1]), names=["Klei_soilphi", "Klei_soilcohesion"])
+    rv_water = MvnRV(mus=np.array([-0.8]), stds=np.array([0.08]), names=["water_lvl"])
+    state = GaussianState(rvs=[rv_strength, rv_water])
 
-    # r = DsheetReliability(model)
-    build_fragility(hunt, state, integration_rv_names=["water"])
+    performance_config = ("max_moment", lambda x: 150. / (x[0] + 1e-5))
+
+    lsf = package_lsf(geomodel, state, performance_config, True)
+
+    build_fragility(lsf, state, integration_rv_names=["water_lvl"], n_integration_grid=3)
+

src/rvs/state.py
@@ -50,11 +50,24 @@ class GaussianState(StateBase):
     def __init__(self, rvs: Annotated[List[MvnRV] | Tuple[MvnRV, ...], "nrvs"]) -> None:
         self.rvs = rvs
         self.names = [*itertools.chain.from_iterable(rv.names for rv in rvs)]
+        self.ndims = len(self.names)
+        self.compile_params()
 
-        mus = [*itertools.chain.from_iterable(rv.mus for rv in rvs)]
-        stds = [*itertools.chain.from_iterable(rv.stds for rv in rvs)]
-        self.marginal_dist = {name: stats.norm(mu, std) for (name, mu, std) in zip(self.names, mus, stds)}
-        self.marginal_dist_type = {name: "normal" for name in self.names}
+        self.jpdf = stats.multivariate_normal(self.mus, self.cov)
+        self.marginal_pdf = {name: stats.norm(mu, std) for (name, mu, std) in zip(self.names, self.mus, self.stds)}
+        self.marginal_pdf_type = {name: "normal" for name in self.names}
+
+    def compile_params(self) -> None:
+        self.mus = np.asarray([*itertools.chain.from_iterable(rv.mus for rv in self.rvs)])
+        self.stds = np.asarray([*itertools.chain.from_iterable(rv.stds for rv in self.rvs)])
+        cov = np.eye(self.ndims)
+        start = 0
+        for rv in self.rvs:
+            end = start + rv.ndims
+            cov[start: end][:, start: end] = rv.cov
+            start = end
+        self.cov = cov
+        self.chol = np.linalg.cholesky(self.cov)
 
     def check_rvs(self) -> None:
         pass
@@ -63,10 +76,16 @@ class GaussianState(StateBase):
         pass
 
     def joint_prob(self, x: EvalInNpType, standard_domain: bool = False) -> float:
-        return np.prod(self.rvs.prob(x, standard_domain=standard_domain))
+        return self.jpdf.pdf(x)
 
     def joint_log_prob(self, x: EvalInNpType, standard_domain: bool = False) -> float:
-        return np.sum(self.rvs.logprob(x, standard_domain=standard_domain))
+        return self.jpdf.logpdf(x)
+
+    def transform(self, x: EvalInNpType) -> EvalInNpType:
+        return self.mus + self.chol.dot(x)
+
+    def detransform(self, x: EvalInNpType) -> EvalInNpType:
+        return np.linalg.inv(self.chol).dot(x-self.mus)
 
 
 if __name__ == "__main__":