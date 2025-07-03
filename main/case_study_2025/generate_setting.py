9bd3940fbaef18254f93ba5dd83a4b4fb5256a67
amavrits <amavrist@gmail.com>
2025-07-03 10:01:48 +0200

    Regenerated setting

--- main/case_study_2025/generate_setting.py
+++ main/case_study_2025/generate_setting.py
@@ -3,13 +3,15 @@ import pandas as pd
 from pathlib import Path
 import json
 import os
+from math import fabs
 from src.geotechnical_models.dsheetpiling.model import DSheetPiling, DSheetPilingResults
 from src.reliability_models.dsheetpiling.lsf import *
+from src.corrosion.corrosion_model import CorrosionModel
 import collections
+from tqdm import tqdm
 
 
-def run_model(rvs, model, rv_names):
-    params = {name: rv for (name, rv) in zip(rv_names, rvs)}
+def run_model(params, model):
     soil_data = unpack_soil_params(params, list(model.soils.keys()))
     water_data = unpack_water_params(params, [lvl.name for lvl in model.water.water_lvls])
     wall_data = unpack_wall_data(params, model.wall._asdict())
@@ -17,43 +19,43 @@ def run_model(rvs, model, rv_names):
     model.update_water(water_data)
     model.update_wall(wall_data)
     model.execute()
-    return model.results.displacement[0]
+    return model.results.displacement[0], model.results.moment[0], model.results.z
 
 
 if __name__ == "__main__":
 
-    times_corrosion = [5, 10, 15, 20]
-    corrosions = [0.8, 0.7, 0.6, 0.5]
-    corrosions = {
-        time: {
-            "corrosion": corrosion,
-            "deformations": []
-        }
-        for (time, corrosion) in zip(times_corrosion, corrosions)
-    }
+    df = pd.read_csv(r"data/true_params.csv", index_col="parameter")
+    true_params = df["value"].to_dict()
 
-    times_deform = [12]
-    df = pd.read_csv(r"data/true_params.csv")
-    param_names = list(df["parameter"])
-    true_params = df["value"].values.tolist()
+    times = list(range(0, 31))
+    corossion_model = CorrosionModel()
+    corrosions = corossion_model.generate_observations(np.array(times), seed=42)
+
     geomodel_path = os.environ["MODEL_PATH"]  # model_path defined as environment variable
     geomodel = DSheetPiling(geomodel_path)
-    deformations = run_model(true_params, geomodel, param_names)
-    idx_locs = list(range(1, 151, 10))
-    deformations = {
-        time: {
-            "corrosion": [],
-            "true_params": {n: t for (n, t) in zip(param_names, true_params)},
-            "deformations_all": deformations,
-            "deformations": [d for i, d in enumerate(deformations) if i in idx_locs],
-            "idx": idx_locs
-        }
-        for (time, deformations) in zip(times_deform, [deformations])
-    }
+    monitoring_locs = list(range(1, 156, 10))
 
-    data = corrosions
-    data.update(deformations)
-    data = collections.OrderedDict(sorted(data.items()))
+    data = {}
+    for i, time in enumerate(tqdm(times)):
+        corrosion = corrosions[i]
+        corrosion_ratio = corrosion / corossion_model.start_thickness
+        EI_corroded = true_params["Wall_SheetPilingElementEI"] * (1 - corrosion_ratio)
+        time_params = deepcopy(true_params)
+        time_params["Wall_SheetPilingElementEI"] = EI_corroded
+        deformations, moments, z = run_model(time_params, geomodel)
+        data[time] = {
+            "time": time,
+            "corrosion": corrosion,
+            "corrosion_ratio": corrosion_ratio,
+            "EI_corroded": EI_corroded,
+            "true_params": true_params,
+            "time_params": time_params,
+            "z": z,
+            "deformations": deformations,
+            "deformations_monitoring": [d for i, d in enumerate(deformations) if i in monitoring_locs],
+            "moments": moments,
+            "max_moment": max([fabs(m) for m in moments])
+        }
 
     path = Path(r"data/setting")
     path.mkdir(parents=True, exist_ok=True)
