aa7a32eb7df0ec2275a355735fa630e4fc5fd3e1
amavrits <amavrist@gmail.com>
2025-07-03 18:23:35 +0200

    Prepared timeline analysis using chebysev surrogate

--- main/case_study_2025/reliability/timeline_analysis.py
+++ main/case_study_2025/reliability/timeline_analysis.py
@@ -1,4 +1,7 @@
 from pathlib import Path
+
+import numpy as np
+
 from main.case_study_2025.reliability.chebysev_reliability import reliability
 from main.case_study_2025.reliability.utils import *
 from src.corrosion.corrosion_model import CorrosionModel
@@ -12,10 +15,13 @@ if __name__ == "__main__":
     mcs_samples_path = SCRIPT_DIR / f"data/mc_samples_normal_100000000.npy"
     chebysev_path = SCRIPT_DIR / "train/results/srg/chebysev/lr_1.0e-05_epochs_100000_fullprofile_True"
 
-    n_mcs = int(mcs_samples_path.stem.split(".")[0].split("_")[-1])
-    fos_calculator = load_chebysev_calculator(chebysev_path, z_path)
+    n_mcs = 100_000
+    # fos_calculator = load_chebysev_calculator(chebysev_path, z_path)
 
     corrosion_model = CorrosionModel()
+
+    moment_cap = 40
+    start_thickness = corrosion_model.start_thickness
 
     with open(setting_path, "r") as f:
         setting_data = json.load(f)
@@ -28,4 +34,5 @@ if __name__ == "__main__":
             break
 
         corrosion_dist = corrosion_model.corrosion_distribution(time)
-
+        np.random.seed(42)
+        corrosions = corrosion_dist.rvs(n_mcs)
