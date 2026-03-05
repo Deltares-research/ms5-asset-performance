bdcef5978a5fe11a0b9f958893564f98198d191d
amavrits <amavrits@gmail.com>
2026-03-05 11:04:08 +0100

    Implemented performance_function.py. Started reliability pipeline.py

--- case_studies/settlement_example/pipeline.py
+++ case_studies/settlement_example/pipeline.py
@@ -1,7 +1,7 @@
 import numpy as np
 from geolib.models.base_model_structure import settings
 from numpy.typing import NDArray
-from case_studies.settlement_example.io import load_json
+from case_studies.settlement_example.io import load_json, get_remote_path
 from jpdf import JPDF
 from performance_function import Performance
 from config import CaseStudyConfig
@@ -28,7 +28,7 @@ class ReliabilityPipeline:
         self.jpdf: Optional[JPDF] = None
         self.performance: Optional[Performance] = None
         self.settlement_at_obs_times: Optional[NDArray] = None
-        self.settlement_at_eval_times: Optional[NDArray] = None
+        self.settlement_at_eval_time: Optional[NDArray] = None
 
         # Results
         self.results: Dict[float, Dict[str, Any]] = {}
@@ -50,44 +50,61 @@ class ReliabilityPipeline:
         params = specs.get("parameters", {})
         self.performance = Performance(name="settlement", parameters=params)
 
-    def eval_settlements(self, setting: Dict[str, float], cache: bool = True) -> None:
-
-
-
+    def init_settlements(
+            self,
+            setting: Dict[str, float],
+            cache_dir: Optional[Path] = None,
+            cache: bool = True
+    ) -> None:
 
         obs_times = [float(key) for key in setting.keys()]
         preload_removal_time = self.config.preload_removal_time
         end_time = self.config.end_time
         times = obs_times + [preload_removal_time, end_time]
 
-        settlements = get_settlement(
-            t=times,
-            CR=self.jpdf.CR_grid,
-            k=self.jpdf.k_grid,
-            RR=self.config.RR,
-            Ca=self.config.Ca,
-            h=self.config.layer_thickness,
-            sigma_0=self.config.sigma_0,
-            sigma_v=self.config.sigma_v,
-            sigma_p=self.config.sigma_p,
-            method=self.config.doc_method,
-        )
+        cache_file = cache_dir / "settlements.npy"
 
-        # If settlement matrix is takes more than 10MB memory, reduce its accuracy.
-        if settlements.nbytes / 1e6 >= 10:
+        if cache and cache_file.exists():
+            
+            settlements = np.load(cache_file)
+            
+            if settlements.shape[-1] != len(times):
+                raise ValueError(f"""
+                Inconsistent times: cached settlements have {settlements.shape[-1]} 
+                times while analysis needs {len(times)}.
+                """)
+            
+        else:
+    
+            settlements = get_settlement(
+                t=times,
+                CR=self.jpdf.CR_grid,
+                k=self.jpdf.k_grid,
+                RR=self.config.RR,
+                Ca=self.config.Ca,
+                h=self.config.layer_thickness,
+                sigma_0=self.config.sigma_0,
+                sigma_v=self.config.sigma_v,
+                sigma_p=self.config.sigma_p,
+                method=self.config.doc_method,
+            )
+
+            np.save(cache_file, settlements)
+
+        # If settlement matrix is takes more than 100MB memory, reduce its accuracy.
+        if settlements.nbytes / 1e6 >= 100:
             settlements = settlements.astype(float32)
             print("Reducing accuracy to of 'settlements' to float32")
             print(f"Current memory for 'settlements'={settlements.nbytes/1e3:.0f}KB")
-
-
-
-        pass
+        
+        self.settlement_at_obs_times = settlements[..., :-2]
+        self.settlement_at_eval_time = settlements[..., -1] - settlements[..., -2]
 
 
 def main():
     # Paths
     load_dotenv("settlement_example.env")
-    os.environ["REMOTE_DATA_PATH"] = os.environ["REMOTE_PATH"] + r"/input"
+    os.environ["REMOTE_DATA_PATH"] = str(get_remote_path()/"input")
     specs_path = Path(os.environ["REMOTE_DATA_PATH"]) / "case_study_specifications.json"
 
     # Initialize pipeline
@@ -109,7 +126,9 @@ def main():
     print("STEP 2: Settlements")
     print("=" * 60)
     setting = load_json("case_study_setting.json")
-    pipeline.eval_settlements(setting=setting, cache=True)
+    cache_dir = get_remote_path() / "output/cache"
+    cache_dir.mkdir(exist_ok=True, parents=True)
+    pipeline.init_settlements(setting=setting, cache=True, cache_dir=cache_dir)
 
 
     pass
